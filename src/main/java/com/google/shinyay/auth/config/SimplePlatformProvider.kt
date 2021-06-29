aad8c54af8ff8f4dcb6ae00c7cbff1dd2ae4ce57
shinyay <shinya.com@gmail.com>
2021-06-29 22:24:05 +0900

    Modify exit process

--- src/main/java/com/google/shinyay/auth/config/SimplePlatformProvider.kt
+++ src/main/java/com/google/shinyay/auth/config/SimplePlatformProvider.kt
@@ -2,9 +2,10 @@ package com.google.shinyay.auth.config
 
 import org.keycloak.platform.PlatformProvider
 import org.keycloak.services.ServicesLogger
+import kotlin.system.exitProcess
 
 class SimplePlatformProvider : PlatformProvider {
-    var shutdownHook: Runnable? = null
+    private var shutdownHook: Runnable? = null
     override fun onStartup(startupHook: Runnable?) {
         startupHook?.run()
     }
@@ -21,7 +22,7 @@ class SimplePlatformProvider : PlatformProvider {
     private fun exit(status: Int) {
         object : Thread() {
             override fun run() {
-                System.exit(status)
+                exitProcess(status)
             }
         }.start()
     }
